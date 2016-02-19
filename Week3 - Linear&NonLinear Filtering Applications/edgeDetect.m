4f11bdd349854f007fa8ca45206225b799ef870f
Sumanth Srinivasan <sumanth.srinivasan@gmail.com>
2016-02-19 12:00:06 -0500

    Fixed edgeDetect to use histogram percentile as threshold

--- Week3 - Linear&NonLinear Filtering Applications/edgeDetect.m
+++ Week3 - Linear&NonLinear Filtering Applications/edgeDetect.m
@@ -7,10 +7,12 @@ Hy = 0.25*[-1 0 1;-2 0 2;-1 0 1];
 fx = computeConv(img,Hx);
 fy = computeConv(img,Hy);
 
-edge = zeros(dim(1),dim(2));
+mag = zeros(dim(1),dim(2));
+
+
 for x=1:dim(1)
     for y=1:dim(2)
-        edge(x,y) = sqrt((fx(x,y)^2)+(fy(x,y)^2));
+        mag(x,y) = sqrt((fx(x,y)^2)+(fy(x,y)^2));
 %         if fx(x,y) >= 15
 %             fx(x,y) = 255;
 %         else
@@ -24,10 +26,70 @@ for x=1:dim(1)
 %         end
     end
 end
-% fx = histEq2(fx);
-% fy = histEq2(fy);
-% imshow(uint8(fx)); 
-% figure; imshow(uint8(fy));figure;
-% imshow(uint8(fx+fy));
-imshow(uint8(edge))
-imwrite(uint8(edge),'edge_lena.bmp');
+% imshow(uint8(mag))
+imwrite(uint8(mag),'edge_lena.bmp');
+
+%  thresholding
+
+sortGrad = sort(sort(mag,1,'descend'),2,'descend');
+sizeGrad = size(sortGrad);
+total = sizeGrad(1)*sizeGrad(2);
+T = input('Enter edge threshold value: ');
+threshVal = 0;
+
+% for x = 1:sizeGrad(1)
+%     for y = sizeGrad(2)
+%         k = x*y;
+%         if ceil((k/total)*100) ==T
+%             threshVal = sortGrad(x,y);
+%         else
+%             continue
+%         end
+%     end
+% end
+breakFlag = 0;
+iter = 1;
+gradHist = computeHist(mag);
+k = dim(1)*dim(2);
+
+gradHist = sort(gradHist,'descend');
+transpose(gradHist);
+cumHist = cumsum(gradHist);
+cumHist = round(ceil(cumHist/cumHist(length(cumHist))*100),-1);
+
+for x = 1:length(gradHist)
+    if cumHist(x) == round(T,-1)
+        disp('x value');
+        disp(x)
+        threshVal = x;
+        
+%         breakFlag = 1;
+        break 
+    end
+end
+% x = 1;
+% while x <= length(sortGrad) && breakFlag == 0
+%     if floor(((x/length(sortGrad))*100)) == T
+%         disp(x)
+%         threshVal = gradHist(x);
+%         breakFlag = 1;  
+%         continue;
+%     end
+%     x = x+1;
+% end
+
+ disp(threshVal);
+ 
+ out = zeros(dim(1),dim(2));
+ for x=1:dim(1)
+    for y=1:dim(2)
+        
+        if mag(x,y) >= threshVal(1);
+            out(x,y) = 255;
+        else
+            out(x,y) = 0;
+        end
+        
+    end
+ end
+imshow(uint8(out));
